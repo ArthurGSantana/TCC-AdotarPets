a979e869acb04b84cea81f8d043f02ecf2b74a2d
Arthur Santana <arthurgiannonisantana@yahoo.com.br>
2021-05-27 22:37:54 -0300

    Realizado algumas modificações e novos componentes

--- Template/src/app/app-routing.module.ts
+++ Template/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { QuemSomosComponent } from './pages/quem-somos/quem-somos.component';
+import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', pathMatch: 'full', redirectTo: 'home'},
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'quem-somos', component: QuemSomosComponent}
 ];
 
 @NgModule({
